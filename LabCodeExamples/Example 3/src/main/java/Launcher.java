71bdd30e35fc36e41192ca070f1af8446d16d59e
Alex <alexx.cosma@gmail.com>
2021-03-07 11:33:52 +0200

    [ACC] - minor changes again

--- LabCodeExamples/Example 3/src/main/java/Launcher.java
+++ LabCodeExamples/Example 3/src/main/java/Launcher.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
  */
 public class Launcher {
 
-    public static boolean BOOTSTRAP = true;
+    public static boolean BOOTSTRAP = false;
 
     public static void main(String[] args) {
         ComponentFactory componentFactory = ComponentFactory.instance(false);
