c1e3df1d12870ec5e214230745c8f9b32c1ded6b
Alex <alexx.cosma@gmail.com>
2021-03-17 19:54:20 +0200

    simplified things

--- LabCodeExamples/Example 4 - Spring #1/src/main/java/com/lab4/demo/DemoApplication.java
+++ LabCodeExamples/Example 4 - Spring #1/src/main/java/com/lab4/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
 public class DemoApplication {
 
     public static void main(String[] args) {
