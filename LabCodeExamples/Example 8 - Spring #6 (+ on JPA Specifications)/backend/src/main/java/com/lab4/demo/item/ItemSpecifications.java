79f789b8d8b719ecbda77d3bc8928dbb1511c972
Alex C <alexandru.cosma@catalysts.cc>
2021-04-18 10:34:49 +0300

    - removed unused dependency

--- LabCodeExamples/Example 8 - Spring #6 (+ on JPA Specifications)/backend/src/main/java/com/lab4/demo/item/ItemSpecifications.java
+++ LabCodeExamples/Example 8 - Spring #6 (+ on JPA Specifications)/backend/src/main/java/com/lab4/demo/item/ItemSpecifications.java
@@ -10,7 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
 import java.time.LocalDateTime;
