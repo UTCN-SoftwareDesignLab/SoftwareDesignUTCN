529048cfe9231f614f25ca7eda97ea18b7c005ed
Alex <alexx.cosma@gmail.com>
2021-04-27 22:24:55 +0300

    - more on grails

--- LabCodeExamples/Example 9 - Grails/grails-app/controllers/example9/grails/UrlMappings.groovy
+++ LabCodeExamples/Example 9 - Grails/grails-app/controllers/example9/grails/UrlMappings.groovy
@@ -9,11 +9,6 @@ class UrlMappings {
             }
         }
 
-        get "/items/$n/$desc"(controller: "item", action: 'byNameAndDescription')
-
-        get "/items/byName/$name"(controller: "item", action: 'byName')
-
-
         "/"(view: "/index")
         "500"(view: '/error')
         "404"(view: '/notFound')
--- LabCodeExamples/Example 9 - Grails/grails-app/controllers/example9/grails/item/ItemController.groovy
+++ LabCodeExamples/Example 9 - Grails/grails-app/controllers/example9/grails/item/ItemController.groovy
@@ -4,25 +4,9 @@ import grails.rest.RestfulController
 
 // https://docs.grails.org/latest/guide/REST.html
 class ItemController extends RestfulController<Item> {
-    static allowedMethods = [byName: "GET", byNameAndDescription: "GET"]
 
     ItemController(Class<Item> resource) {
         super(resource)
-    }
-
-    def byName(String name) {
-        respond Item.findAllByNameLike(wildcard(name))
-    }
-
-    def byNameAndDescription(String n, String desc) {
-        respond Item.whereAny {
-            name.contains(n)
-            description.contains(desc)
-        }.findAll()
-    }
-
-    private String wildcard(String str) {
-        '%' + str + '%'
     }
 
 }
