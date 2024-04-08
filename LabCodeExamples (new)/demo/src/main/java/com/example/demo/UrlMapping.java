82f800910ccf47d6b5ce937ca98228bbda752ad9
alexcosma <alexandru.cosma@cloudflight.io>
2024-04-09 00:39:03 +0300

    - removed stuff

--- LabCodeExamples (new)/demo/src/main/java/com/example/demo/UrlMapping.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class UrlMapping {
-  public static final String API = "/api";
-  public static final String ITEMS = API + "/item";
-  public static final String SECOND_METHOD = "/second-method";
-}
