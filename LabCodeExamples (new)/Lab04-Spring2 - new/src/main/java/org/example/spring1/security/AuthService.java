5a5014d3f282066a6e522c61ca4779982d0e2765
alexcosma <alexandru.cosma@cloudflight.io>
2024-04-08 23:17:46 +0300

    - added code

---
+++ LabCodeExamples (new)/Lab04-Spring2 - new/src/main/java/org/example/spring1/security/AuthService.java
@@ -0,0 +1,57 @@
+package org.example.spring1.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.spring1.security.dto.SignupRequest;
+import org.example.spring1.user.RoleRepository;
+import org.example.spring1.user.UserRepository;
+import org.example.spring1.user.model.ERole;
+import org.example.spring1.user.model.Role;
+import org.example.spring1.user.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+  private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder encoder;
+
+  public boolean existsByUsername(String username) {
+    return userRepository.existsByUsername(username);
+  }
+
+  public boolean existsByEmail(String email) {
+    return userRepository.existsByEmail(email);
+  }
+
+  public void register(SignupRequest signUpRequest) {
+    User user = User.builder()
+        .username(signUpRequest.getUsername())
+        .password(encoder.encode(signUpRequest.getPassword()))
+        .email(signUpRequest.getEmail())
+        .build();
+
+    Set<String> rolesStr = signUpRequest.getRoles();
+    Set<Role> roles = new HashSet<>();
+
+    if (rolesStr == null) {
+      Role defaultRole = roleRepository.findByName(ERole.CUSTOMER)
+          .orElseThrow(() -> new RuntimeException("Cannot find CUSTOMER role"));
+      roles.add(defaultRole);
+    } else {
+      rolesStr.forEach(r -> {
+        Role ro = roleRepository.findByName(ERole.valueOf(r))
+            .orElseThrow(() -> new RuntimeException("Cannot find role: " + r));
+        roles.add(ro);
+      });
+    }
+
+    user.setRoles(roles);
+    userRepository.save(user);
+  }
+}
---
+++ LabCodeExamples (new)/Lab04-Spring2/src/main/java/org/example/spring1/security/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package org.example.spring1.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.example.spring1.UrlMapping.AUTH;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+  private final UserDetailsService userDetailsService;
+  private final AuthTokenFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeHttpRequests(request -> request.requestMatchers(AUTH).permitAll()
+            .anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("http://localhost:4200").allowCredentials(true);
+      }
+    };
+  }
+}
